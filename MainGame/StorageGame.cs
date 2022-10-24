352007b9cfbc0281cdd663d5bb2ae5c40249475c
Ardea1 <cmad1@mail.ru>
2022-10-25 00:34:12 +0700

    0:34

--- MainGame/StorageGame.cs
+++ MainGame/StorageGame.cs
@@ -10,6 +10,9 @@ namespace PsychicsGame.MainGame
     {
         public Game game = new Game();
 
+        // Здесь Я хотела написать логику соранения
+        // и загрузки игры. Но не знаю, как это сделать.
+        // Я прочла много о сессии, но не могу вернуть нужный мне момент игры.
         public void SaveGame (Game game)
         {
             HttpContext.Current.Session["game"] = game;
